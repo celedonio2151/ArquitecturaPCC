cfa66278bb96c2773c97ef13f0d41b27b4a7277c
celedonio <cele2025.tecnology@gmail.com>
2021-05-11 14:09:37 -0400

    Subiendo el tercer ejercicio

---
+++ Zoologico/Clase.php
@@ -0,0 +1,37 @@
+<?php
+    /**
+     *
+     */
+    class Zoologico
+    {
+        private $Nombre;
+        private $ListaDeAlumnos;
+        private $Direccion;
+        private $Telefono;
+        private $Correo;
+        function __construct(argument)
+        {
+            $this->ListaDeAlumnos = array();
+        }
+
+        function setDatos($Nombre,$ListaJaulas,$Direccion,$Telefono,$Correo)
+        {
+            // Coloca los datos del zoologico
+        }
+        function imprimir()
+        {
+            // Muestra los datos del zoologico
+        }
+
+
+        function insertarAnimal()
+        {
+            // Permite insertar un animal al Zoologico
+        }
+        function mostrarListaAnimales()
+        {
+            // Muestra todo el listado de animales
+        }
+    }
+
+ ?>
